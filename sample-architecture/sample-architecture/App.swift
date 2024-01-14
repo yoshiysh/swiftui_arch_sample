8d38a50eec4a36aa64fffc5c00ead0e4c33abe1e
yoshiysh <yoshiki.henmi@gmail.com>
2024-01-14 14:58:39 +0900

    Initial commit.

---
+++ sample-architecture/sample-architecture/App.swift
@@ -0,0 +1,17 @@
+//
+//  sample_architectureApp.swift
+//  sample-architecture
+//
+//  Created by yoshi on 2024/01/14.
+//
+
+import SwiftUI
+
+@main
+struct app: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
